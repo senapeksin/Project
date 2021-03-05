46cd43ffc7458914228014bee85da0c9de2d58a8
SENA <senapksnn@gmail.com>
2021-03-06 00:40:25 +0300

    commit

--- Business/Concrete/.vshistory/ProductManager.cs/2021-03-04_01_24_34_877.cs
+++ Business/Concrete/.vshistory/ProductManager.cs/2021-03-04_01_24_34_877.cs
@@ -1,7 +1,6 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
-using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -25,12 +24,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             //iş kodları -- business code
             //validation -- doğrulama
-           
+            ValidationTool.Validate(new ProductValidator(),product);
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
--- Business/DependencyResolvers/Autofac/.vshistory/AutofacBusinessModule.cs/2021-03-06_00_37_54_036.cs
+++ Business/DependencyResolvers/Autofac/.vshistory/AutofacBusinessModule.cs/2021-03-06_00_37_54_036.cs
@@ -1,9 +1,6 @@
 ﻿using Autofac;
-using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
-using Castle.DynamicProxy;
-using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
