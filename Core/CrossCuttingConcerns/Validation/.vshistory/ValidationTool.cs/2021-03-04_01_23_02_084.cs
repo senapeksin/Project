36feb9d2879a767eeade4dd52a4ee08b1e259a81
SENA <senapksnn@gmail.com>
2021-03-11 00:11:19 +0300

    commit

--- Core/CrossCuttingConcerns/Validation/.vshistory/ValidationTool.cs/2021-03-04_01_23_02_084.cs
+++ Core/CrossCuttingConcerns/Validation/.vshistory/ValidationTool.cs/2021-03-04_01_23_02_084.cs
@@ -7,7 +7,6 @@ namespace Core.CrossCuttingConcerns.Validation
 {
     public static class ValidationTool
     {
-        //validator : Doğrulama kurallarının olduğu sınıf, entity : doğrulanacak sınıf
         public static void Validate(IValidator validator,object entity)
         {
             var context = new ValidationContext<object>(entity);
